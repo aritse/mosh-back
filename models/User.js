8be819fd7ac1810009bec3f1213df2c55147035d
jittel <nathan.c.ginn@gmail.com>
2020-02-14 11:11:12 -0800

    updated user model

--- models/User.js
+++ models/User.js
@@ -1,11 +1,35 @@
+//npm package import, bcrypt does the encrypting for us
+var bcrypt = require('bcrypt');
+​
 module.exports = function(sequelize, DataTypes) {
-  const User = sequelize.define("User", {
-    email: DataTypes.STRING,
-    password: DataTypes.STRING
-  });
-
-  return User;
+    var User = sequelize.define('User', {
+        email: {
+            type:DataTypes.STRING,
+            allowNull:false,
+            unique:true,
+            validate: {
+              len: [1],
+              isEmail: true
+          }
+          },
+          password: {
+            type:DataTypes.STRING,
+            allowNull:false,
+            validate:{
+                len:[8]
+            }
+        }
+    });
+​
+    User.associate = function(models) {
+        // add associations here
+        // ex:User.hasMany(models.BlogPost);
+    };
+    //sequelize hook, will run before model instance is created and hash password
+    User.beforeCreate(function(user) {
+        user.password = bcrypt.hashSync(user.password, bcrypt.genSaltSync(10), null);
+      });
+    
+​
+    return User;
 };
-
-// todo: add encryption
-// todo: add validation
