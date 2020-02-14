b3fcbe0eb1e341fdf7309cbe8067dffa2661b1dd
jittel <nathan.c.ginn@gmail.com>
2020-02-14 11:22:16 -0800

    fixed up ports and added routing for auth

--- index.js
+++ index.js
@@ -1,16 +1,57 @@
-const express = require("express");
+var express = require('express');
+const cors = require("cors");
+const bcrypt = require("bcrypt");
+const session = require("express-session")
+// Sets up the Express App
 
-const app = express();
+// =============================================================
+var app = express();
+var PORT = process.env.PORT || 8080;
 
+app.use(cors({
+  origin: ["http://localhost:3000"],
+  credentials: true
+}));
+app.use(session({ secret: "something secret here", resave: true, saveUninitialized: true,cookie:{maxAge: 7200000} }));
+// app.use(cors({
+//     origin:["https://*OUR APP NAME HERE*.herokuapp.com"]
+// }));
+
+// Requiring our models for syncing
+var db = require('./models');
+
+// Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 const routes = require("./routes");
 app.use("/api", routes);
 
-const port = 8080;
+app.post("/api/auth/signup", (req, res) => {
+  db.User.create(req.body).then(userData => {
+    res.json(userData);
+  })
+})
 
-const db = require("./models");
+app.post("/api/auth/login", (req, res) => {
+  db.User.findOne({
+    where: {
+      name: req.body.name
+    }
+  }).then(dbUser=>{
+    if(bcrypt.compareSync(req.body.password,dbUser.password)){
+      req.session.user={
+        id:dbUser.id,
+        name:dbUser.name
+      }
+      res.json(req.session.user)
+    }
+    else{
+      res.status(401).json("not logged in")
+    }
+  })
+})
+
 db.sequelize.sync({ force: false }).then(() => {
-  app.listen(port, console.log(`express app is listening on http://localhost:${port}`));
+  app.listen(port, console.log(`express app is listening on http://localhost:${PORT}`));
 });
