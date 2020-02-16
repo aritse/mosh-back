18bf50700332daecdc44dee18647cfa10b738edc
unknown <nathan.c.ginn@gmail.com>
2020-02-15 18:11:26 -0800

    auth routes and controllers working

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,6 +3,40 @@ const db = require("./models");
 
 module.exports = {
     signUp: function (req, res) {
-        //
+        db.User.create(req.body).then(userData => {
+            res.json(userData);
+        })
+    },
+    logIn: function (req, res) {
+        db.User.findOne({
+            where: {
+                email: req.body.email
+            }
+        }).then(dbUser => {
+            if (bcrypt.compareSync(req.body.password, dbUser.password)) {
+                req.session.user = {
+                    id: dbUser.id,
+                    email: dbUser.email
+                }
+                res.json(req.session.user)
+            }
+            else {
+                res.status(401).json("not logged in")
+            }
+        })
+    },
+    loggedInUser: function (req, res) {
+        if (req.session.user) {
+            res.json(req.session.user)
+        } else {
+            res.status(401).json("not logged in")
+        }
+    },
+    logOut: function (req, res) {
+        //delete session user, logging you out
+        req.session.destroy(function () {
+            res.send('successfully logged out')
+            // res.render("home");
+        })
     }
 }
--- routes/api/authRoutes.js
+++ routes/api/authRoutes.js
@@ -1,47 +1,10 @@
-const express = require("express");
-const router = express.Router();
-const bcrypt = require("bcrypt");
-const db = require("./models");
+const router = require("express").Router();
+const authController = require("../../controllers/authController");
 
-router.post("/auth/signup", (req, res) => {
-    db.User.create(req.body).then(userData => {
-        res.json(userData);
-    })
-})
-
-router.post("/auth/login", (req, res) => {
-    db.User.findOne({
-        where: {
-            email: req.body.email
-        }
-    }).then(dbUser => {
-        if (bcrypt.compareSync(req.body.password, dbUser.password)) {
-            req.session.user = {
-                id: dbUser.id,
-                email: dbUser.email
-            }
-            res.json(req.session.user)
-        }
-        else {
-            res.status(401).json("not logged in")
-        }
-    })
-})
-
-router.get('/auth/loggedinuser', (req, res) => {
-    if (req.session.user) {
-        res.json(req.session.user)
-    } else {
-        res.status(401).json("not logged in")
-    }
-})
-
-router.get('/logout', function (req, res) {
-    //delete session user, logging you out
-    req.session.destroy(function () {
-        res.send('successfully logged out')
-        // res.render("home");
-    })
-})
+// route to this point: /user
+router.route("/signup").post(authController.signUp);
+router.route("/login").post(authController.logIn);
+router.route("/loggedinuser").get(authController.loggedInUser)
+router.route("/logout").get(authController.logOut);
 
 module.exports = router;
