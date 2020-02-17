24f0454cc862855cd0352b91067c2ffb228068f9
unknown <nathan.c.ginn@gmail.com>
2020-02-17 12:05:58 -0800

    changed a comment

--- routes/api/authRoutes.js
+++ routes/api/authRoutes.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 const authController = require("../../controllers/authController");
 
-// route to this point: /user
+// route to this point: /auth
 router.route("/signup").post(authController.signUp);
 router.route("/login").post(authController.logIn);
 router.route("/loggedinuser").get(authController.loggedInUser)
