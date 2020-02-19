3d0316f8f0666e51f9a39b8fc3e244db8560469c
chrisp-dev <chris.paradeise@gmail.com>
2020-02-19 10:59:19 -0800

    Added swipeRoutes to api routes

--- routes/api/index.js
+++ routes/api/index.js
@@ -3,10 +3,12 @@ var router = express.Router();
 var userRoutes = require("./userRoutes");
 var authRoutes = require("./authRoutes");
 var chatRoutes = require("./chatRoutes");
+var swipeRoutes = require("./swipeRoutes");
 
 // route to this point: /api
 router.use("/user", userRoutes);
 router.use("/auth", authRoutes);
 router.use("/chat", chatRoutes);
+router.use("/swipe", swipeRoutes);
 
 module.exports = router;
