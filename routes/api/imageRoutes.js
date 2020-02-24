cfa217b6d652d26cd74b019f2818094316266758
jittel <nathan.c.ginn@gmail.com>
2020-02-24 10:37:51 -0800

    image upload works

--- routes/api/imageRoutes.js
+++ routes/api/imageRoutes.js
@@ -3,5 +3,6 @@ const imageController = require("../../controllers/imageController");
 
 // route to get to this point: /image
 router.route("/upload").post(imageController.uploadImage);
+router.route("/:id").get(imageController.singleImage);
 
 module.exports = router;
