a8b8e144821428474160924ffb48c7de970f9239
Ari Tserendavaa <tserenda@hotmail.com>
2020-02-24 14:02:38 -0800

    updated the cors origin

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.use(express.json());
 app.use(express.static(__dirname + "/public"));
 app.use(
   cors({
-    origin: ["https://moshbook2.herokuapp.com"],
+    origin: ["https://moshsocial.herokuapp.com"],
     credentials: true
   })
 );
