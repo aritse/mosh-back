495883a954a75326e1a3b23677858b70d4d17552
chrisp-dev <chris.paradeise@gmail.com>
2020-02-27 16:19:52 -0800

    Attempted to fix "trim of undefined" error Chat should default to channel 99 getChatMessages api call added, this will get where logged in user is both receiver or sender. More seeds added.

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -3,69 +3,81 @@ const db = require("../models");
 module.exports = {
   saveMessage: (req, res) => db.Message.create(req.body).then(message => res.json(message)),
 
-  getSenderMessages: function(req, res) {
+  getSenderMessages: function (req, res) {
     console.log("got sender id:", req.params.id);
     db.Message.findAll({
       where: {
         senderId: req.params.id
       }
     })
-      .then(function(dbMessages) {
+      .then(function (dbMessages) {
         res.json(dbMessages);
       })
-      .catch(function(err) {
+      .catch(function (err) {
         console.error(err);
       });
   },
-  getReceiverMessages: function(req, res) {
+  getChatMessages: async function (req, res) {
+    try {
+      const [chatMessages, metadata] = await db.sequelize.query(
+        `SELECT *
+              FROM Messages 
+              WHERE receiverId = ${req.session.user.id || 1} OR senderId = ${req.session.user.id};
+          `
+      )
+    } catch (e) {
+      res.json(e.message);
+    }
+  },
+  getReceiverMessages: function (req, res) {
     db.Message.findAll({
       where: {
         receiverId: req.session.user.id
       }
     })
-      .then(function(dbMessages) {
+      .then(function (dbMessages) {
         res.json(dbMessages);
       })
-      .catch(function(err) {
+      .catch(function (err) {
         console.error(err);
       });
   },
-  deleteSingleMessage: function(req, res) {
+  deleteSingleMessage: function (req, res) {
     db.Message.destroy({
       where: {
         id: req.params.id
       }
     })
-      .then(function(data) {
+      .then(function (data) {
         res.json(data);
       })
-      .catch(function(err) {
+      .catch(function (err) {
         console.error(err);
       });
   },
-  deleteSenderMessages: function(req, res) {
+  deleteSenderMessages: function (req, res) {
     db.Message.destroy({
       where: {
         senderId: req.params.id
       }
     })
-      .then(function(data) {
+      .then(function (data) {
         res.json(data);
       })
-      .catch(function(err) {
+      .catch(function (err) {
         console.error(err);
       });
   },
-  deleteReceiverMessages: function(req, res) {
+  deleteReceiverMessages: function (req, res) {
     db.Message.destroy({
       where: {
         receiverId: req.params.id
       }
     })
-      .then(function(data) {
+      .then(function (data) {
         res.json(data);
       })
-      .catch(function(err) {
+      .catch(function (err) {
         console.error(err);
       });
   }
